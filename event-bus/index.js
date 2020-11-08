6ab82de7af35254337c06f0df911beb140e170ac
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-07 16:04:57 -0800

    Implement sync data for query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,10 +5,14 @@ const axios = require('axios')
 const app = express();
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req,res) => {
     console.log('event received', req.body.type);
 
     const event = req.body;
+
+    events.push(event)
 
     axios.post('http://localhost:4000/events', event).then(result => {}).catch(err => console.log(err)).finally(console.log('ran 4000'))
     axios.post('http://localhost:4001/events', event).then(result => {}).catch(err => console.log(err)).finally(console.log('ran 4001'))
@@ -17,6 +21,11 @@ app.post('/events', (req,res) => {
 
     res.send({ status: 'OK'});
 
+})
+
+// Method to get all events. In case service goes down or we start a new service we can use this to sync all services
+app.get('/events', (req,res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
